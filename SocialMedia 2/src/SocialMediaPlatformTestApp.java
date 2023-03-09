aa884f6b09cccd260293999099ee86d1aaef6457
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-03-09 17:19:18 +0000

    Add files via upload

---
+++ SocialMedia 2/src/SocialMediaPlatformTestApp.java
@@ -0,0 +1,52 @@
+import socialmedia.AccountIDNotRecognisedException;
+import socialmedia.BadSocialMedia;
+import socialmedia.IllegalHandleException;
+import socialmedia.InvalidHandleException;
+import socialmedia.SocialMediaPlatform;
+
+/**
+ * A short program to illustrate an app testing some minimal functionality of a
+ * concrete implementation of the SocialMediaPlatform interface -- note you will
+ * want to increase these checks, and run it on your SocialMedia class (not the
+ * BadSocialMedia class).
+ *
+ * 
+ * @author Diogo Pacheco
+ * @version 1.0
+ */
+public class SocialMediaPlatformTestApp {
+
+	/**
+	 * Test method.
+	 * 
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		System.out.println("The system compiled and started the execution...");
+
+		SocialMediaPlatform platform = new BadSocialMedia();
+
+		assert (platform.getNumberOfAccounts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
+		assert (platform.getTotalOriginalPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
+		assert (platform.getTotalCommentPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
+		assert (platform.getTotalEndorsmentPosts() == 0) : "Innitial SocialMediaPlatform not empty as required.";
+
+		Integer id;
+		try {
+			id = platform.createAccount("my_handle");
+			assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered in the system does not match";
+
+			platform.removeAccount(id);
+			assert (platform.getNumberOfAccounts() == 0) : "number of accounts registered in the system does not match";
+
+		} catch (IllegalHandleException e) {
+			assert (false) : "IllegalHandleException thrown incorrectly";
+		} catch (InvalidHandleException e) {
+			assert (false) : "InvalidHandleException thrown incorrectly";
+		} catch (AccountIDNotRecognisedException e) {
+			assert (false) : "AccountIDNotRecognizedException thrown incorrectly";
+		}
+
+	}
+
+}
---
+++ SocialMedia 2/src/socialmedia/BadMiniSocialMedia.java
@@ -0,0 +1,108 @@
+package socialmedia;
+
+import java.io.IOException;
+
+/**
+ * BadMiniSocialMedia is a minimally compiling, but non-functioning implementor
+ * of the MiniSocialMediaPlatform interface.
+ * 
+ * @author Diogo Pacheco
+ * @version 1.0
+ */
+public class BadMiniSocialMedia implements MiniSocialMediaPlatform {
+
+	@Override
+	public int createAccount(String handle) throws IllegalHandleException, InvalidHandleException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeAccount(int id) throws AccountIDNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void changeAccountHandle(String oldHandle, String newHandle)
+			throws HandleNotRecognisedException, IllegalHandleException, InvalidHandleException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String showAccount(String handle) throws HandleNotRecognisedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createPost(String handle, String message) throws HandleNotRecognisedException, InvalidPostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int endorsePost(String handle, int id)
+			throws HandleNotRecognisedException, PostIDNotRecognisedException, NotActionablePostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int commentPost(String handle, int id, String message) throws HandleNotRecognisedException,
+			PostIDNotRecognisedException, NotActionablePostException, InvalidPostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deletePost(int id) throws PostIDNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String showIndividualPost(int id) throws PostIDNotRecognisedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StringBuilder showPostChildrenDetails(int id)
+			throws PostIDNotRecognisedException, NotActionablePostException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMostEndorsedPost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMostEndorsedAccount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void erasePlatform() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void savePlatform(String filename) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadPlatform(String filename) throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
