544235093158d0ca5eb87dcf3d3cc89f1deb8a3b
jeremy-shr <117347026+jeremy-shr@users.noreply.github.com>
2023-03-13 14:52:52 +0000

    Update Comment.java

--- SocialMedia 2/src/socialmedia/Comment.java
+++ SocialMedia 2/src/socialmedia/Comment.java
@@ -1,5 +1,18 @@
 package socialmedia;
 
-public class Comment {
+public class Comment extends Post {
+    User author;
+    int id;
+    Post originalPost;
+    String message;
 
+    public User getAuthor() {
+        return this.author;
+    }
+
+    public Comment(User author, String message, Post original) {
+        super(author, message);
+        this.originalPost = original;
+        this.message = "Commenting on post #" + original.getId();
+    }
 }
