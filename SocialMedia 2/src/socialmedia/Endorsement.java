38d2dd06ecc4bec71b9f601dfc4ba46367adfa7f
jeremy-shr <117347026+jeremy-shr@users.noreply.github.com>
2023-03-27 10:59:20 +0100

    Update Endorsement.java

--- SocialMedia 2/src/socialmedia/Endorsement.java
+++ SocialMedia 2/src/socialmedia/Endorsement.java
@@ -1,13 +1,16 @@
 package socialmedia;
 
 public class Endorsement extends Post {
-
-    User author;
-    int id;
-    String message;
+    Post originalPost;
 
     public Endorsement(User author, Post original, SocialMedia socialmedia) {
         super(author, "Empty Endorsement", socialmedia);
+        this.originalPost = original;
         this.message = "EP@" + author.getHandle() + ": " + original.getMessage();
+    }
+
+    public void deleteEndorsement() {
+        originalPost.postEndorsments.remove(this);
+        this.author.userEndorsements.remove(this);
     }
 }
