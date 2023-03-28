67a6ca7ebc7a179f6bf63247bb497ceb9f074930
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-03-29 00:33:32 +0100

    Update Post.java
    ---NEEDS CHECKING---

--- SocialMedia 2/src/socialmedia/Post.java
+++ SocialMedia 2/src/socialmedia/Post.java
@@ -2,6 +2,13 @@ package socialmedia;
 
 import java.util.ArrayList;
 
+/**
+ * Post is the parent class for endorsment and comment and
+ * is used to create a actionable post on our social media 
+ *
+ * @author Jeremy & Maxime
+ * @version 1.0
+ */
 public class Post {
     int id;
     User author;
@@ -10,30 +17,41 @@ public class Post {
 
     ArrayList<Endorsement> postEndorsments = new ArrayList<>();
     ArrayList<Comment> postComments = new ArrayList<>();
-
-    public int getId() {
-        return this.id;
-    }
-
-    public int getNumberOfEndorsments() {
-        return this.postEndorsments.size();
-    }
-
-    public int getNumberOfComments() {
-        return this.totalNumberOfComments;
-    }
-
+    
+    /** the constructor class for Post to get a author, 
+    a messsage and the socialmedia object*
+    
+    @param author the author of the post
+    @param message the message inputed by the user for the post
+    @param socialmedia the socialmedia object */
     public Post(User author, String message, SocialMedia socialmedia) {
         this.author = author;
         this.message = message;
         this.id = socialmedia.getPostCount();
         socialmedia.postCount++;
     }
-
+    /** Method to get the post Id
+        @return the numerical id **/
+    public int getId() {
+        return this.id;
+    }
+    /** Method to get the numberof endorsments of the post
+        @return the number of endorsments **/
+    public int getNumberOfEndorsments() {
+        return this.postEndorsments.size();
+    }
+    /** Method to get the number of comments of this post
+        @return the number of comments **/
+    public int getNumberOfComments() {
+        return this.totalNumberOfComments;
+    }
+    /** Method to get the message inpute by the user
+        @return the message string **/
     public String getMessage() {
         return this.message;
     }
-
+    /** Method to get th author of the post
+        @return the author **/
     public User getAuthor() {
         return this.author;
     }
