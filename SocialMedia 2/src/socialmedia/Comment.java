e682ddb324908ddd3f89258fa62acf8c0b0621ae
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-03-29 00:00:11 +0100

    Update Comment.java
    ---NEED CHECKING---
    -missing : socialmedia param 
    adding the documentation for this file

--- SocialMedia 2/src/socialmedia/Comment.java
+++ SocialMedia 2/src/socialmedia/Comment.java
@@ -1,10 +1,32 @@
 package socialmedia;
 
+/**
+ * Comment is a childclass of Post used to generate comments 
+ * with implemented methods and variables 
+ *
+ * @author Jeremy & Maxime
+ * @version 1.0
+ */
+
+
 public class Comment extends Post {
+    
+    /** Creates two reference variables to obtain the original Post
+        And in the case of a comment on a comment get the previous one  **/
+    
     Post originalPost;
     Post pointer;
 
     public Comment(User author, String message, Post previous, SocialMedia socialmedia) {
+        
+        /** 
+        Creates the constructor for the Comment class and adds to the TotalNumberOfComments counter 
+        
+        @param author the author of the comment
+        @param message the message of the comment
+        @param previous the previous post or comment you are commenting on
+        @param socialmedia**/
+
         super(author, message, socialmedia);
         this.pointer = previous;
         this.message = message;
@@ -14,12 +36,16 @@ public class Comment extends Post {
     }
 
     public void deleteComment() {
+        
+        /** Method to use when deleting a comment **/
         this.author.userComments.remove(this);
         this.originalPost.postComments.remove(this);
         this.originalPost.totalNumberOfComments--;
     }
 
     public Post getOriginalPost() {
+        
+        /** Method to get the original commented post **/
         return this.originalPost;
     }
 }
