2fcff24cca8dac661bef35823c458a8276903896
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2023-03-29 00:10:29 +0100

    Update Comment.java

--- SocialMedia 2/src/socialmedia/Comment.java
+++ SocialMedia 2/src/socialmedia/Comment.java
@@ -1,9 +1,25 @@
 package socialmedia;
 
+/**
+ * Comment is a childclass of Post used to generate comments 
+ * with implemented methods and variables 
+ *
+ * @author Jeremy & Maxime
+ * @version 1.0
+ */
+
+
 public class Comment extends Post {
+
     Post originalPost;
     Post pointer;
-
+      /** 
+      Creates the constructor for the Comment class and adds to the TotalNumberOfComments counter 
+        
+      @param author the author of the comment
+      @param message the message of the comment
+      @param previous the previous post or comment you are commenting on
+      @param socialmedia **/
     public Comment(User author, String message, Post previous, SocialMedia socialmedia) {
         super(author, message, socialmedia);
         this.pointer = previous;
@@ -12,13 +28,13 @@ public class Comment extends Post {
         this.originalPost.totalNumberOfComments++;
         this.pointer.totalNumberOfComments++;
     }
-
+    /** Method to use when trying to delete a comment **/
     public void deleteComment() {
         this.author.userComments.remove(this);
         this.originalPost.postComments.remove(this);
         this.originalPost.totalNumberOfComments--;
     }
-
+    /** Method to get original post commenting on **/
     public Post getOriginalPost() {
         return this.originalPost;
     }
