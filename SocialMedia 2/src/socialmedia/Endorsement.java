ef9c4213530091d24e9b4e187a9003ad8ecc7477
jeremy-shr <117347026+jeremy-shr@users.noreply.github.com>
2023-03-30 19:53:18 +0100

    Update Endorsement.java

--- SocialMedia 2/src/socialmedia/Endorsement.java
+++ SocialMedia 2/src/socialmedia/Endorsement.java
@@ -1,28 +1,32 @@
 package socialmedia;
 
 /**
- * Endorsment is a childclass of Post used to generate endorsments 
- * with implemented methods and variables. It is used to get an original post 
- * and create an endorsment post for this post
+ * Endorsment is a childclass of Post used to generate endorsements
+ * with implemented methods and variables. It is used to create an endorsment
+ * post for a given original post. Instances of this class are endorsements.
  *
  * @author Jeremy & Maxime
  * @version 1.0
  */
-
 public class Endorsement extends Post {
-    
     Post originalPost;
-    /** Endorsment constuctor class gets the original post and 
-    creates an endorsment post with message format
-    
-    @param author author of the endorsment
-    @param original the original post being endorsed
-    @param socialmedia the socialmedia object **/
+
+    /**
+     * Endorsment constuctor class gets the original post and
+     * creates an endorsment post with message format
+     * 
+     * @param author      author of the endorsment
+     * @param original    the original post being endorsed
+     * @param socialmedia the socialmedia object
+     **/
     public Endorsement(User author, Post original, SocialMedia socialmedia) {
         super(author, "Empty Endorsement", socialmedia);
         this.originalPost = original;
         this.message = "EP@" + author.getHandle() + ": " + original.getMessage();
+        this.originalPost.postEndorsments.add(this);
+        this.author.userEndorsements.add(this);
     }
+
     /** Method to delete endorsments **/
     public void deleteEndorsement() {
         originalPost.postEndorsments.remove(this);
