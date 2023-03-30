ed2fa296e9071c349d8b9e11cb5b902de96d9a5b
jeremy-shr <117347026+jeremy-shr@users.noreply.github.com>
2023-03-30 19:52:57 +0100

    Update Comment.java

--- SocialMedia 2/src/socialmedia/Comment.java
+++ SocialMedia 2/src/socialmedia/Comment.java
@@ -1,41 +1,57 @@
 package socialmedia;
 
 /**
- * Comment is a childclass of Post used to generate comments 
- * with implemented methods and variables 
+ * Comment is a childclass of Post used to generate comments
+ * with implemented methods and variables
  *
  * @author Jeremy & Maxime
  * @version 1.0
  */
 
-
 public class Comment extends Post {
-
     Post originalPost;
     Post pointer;
-      /** 
-      Creates the constructor for the Comment class and adds to the TotalNumberOfComments counter 
-        
-      @param author the author of the comment
-      @param message the message of the comment
-      @param previous the previous post or comment you are commenting on
-      @param socialmedia the socialmedia object**/
+
+    /**
+     * Constructor method for the Comment class, adds to the TotalNumberOfComments
+     * counter
+     * 
+     * @param author      the author of the comment
+     * @param message     the message of the comment
+     * @param previous    the previous post or comment you are commenting on
+     * @param socialmedia the socialmedia object
+     **/
     public Comment(User author, String message, Post previous, SocialMedia socialmedia) {
         super(author, message, socialmedia);
         this.pointer = previous;
         this.message = message;
+        this.pointer.isLast = false;
+        this.author.userComments.add(this);
+        socialmedia.allComments.add(this);
         previous.postComments.add(this);
-        this.originalPost.totalNumberOfComments++;
-        this.pointer.totalNumberOfComments++;
     }
+
     /** Method to use when trying to delete a comment **/
     public void deleteComment() {
-        this.author.userComments.remove(this);
-        this.originalPost.postComments.remove(this);
-        this.originalPost.totalNumberOfComments--;
+        if (this.originalPost == null && this.pointer == null) {
+            this.author.userComments.remove(this);
+        } else if (this.originalPost == null) {
+            this.author.userComments.remove(this);
+            this.pointer.totalNumberOfComments--;
+        } else {
+            this.author.userComments.remove(this);
+            this.originalPost.postComments.remove(this);
+            this.originalPost.totalNumberOfComments--;
+            this.pointer.totalNumberOfComments--;
+        }
+        return;
     }
-    /** Method to get original post commenting on 
-        @return the original post **/
+
+    /**
+     * Method to get original post commenting on
+     * 
+     * @return the original post object
+     **/
     public Post getOriginalPost() {
         return this.originalPost;
     }
